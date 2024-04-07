02d02cad410baca4c99bb1cabf3402111000afd7
Lakshmi_N <lakshmi.rajyam06@gmail.com>
2024-04-07 11:52:50 +0200

    My Terraform Files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "Nalla06"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "linux-key-pair"
+}
+
+variable "hosted-zone" {
+  default = "nallalakshmirajyam.click"
+}
